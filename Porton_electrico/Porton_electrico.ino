19f04df369c6ac71db3803516d198cff7aff0e55
ramun9533 <ramun346416@gmail.com>
2020-11-02 03:15:53 -0600

    se corrige estetica

--- Porton_electrico/Porton_electrico.ino
+++ Porton_electrico/Porton_electrico.ino
@@ -15,28 +15,23 @@ static unsigned int PinArranque = 2;
 static unsigned int PinParo = 3;
 static unsigned int Pinreversible = 4;
 static unsigned int Pinw = 5;
-static unsigned int Pinestado=7;
-static unsigned int Pinrestado=8;
-static unsigned int Pinprot=9;
+static unsigned int Pinestado = 7;
+static unsigned int Pinrestado = 8;
+static unsigned int Pinprot = 9;
 void setup () {
 
   BTSerial.begin(9600);//Inicializar comunicacion
-
   digitalWrite (Pinprot, HIGH);
-
   pinMode(PinArranque, INPUT);
   pinMode(PinParo, INPUT);
   pinMode(Pinreversible, INPUT);
   pinMode(Pinw, INPUT);
   pinMode(Pinprot, OUTPUT);
   pinMode(Led, OUTPUT);
- delay (1000);
-  
-digitalWrite (Pinprot, LOW);
-pinMode(Pinestado, OUTPUT);
+  delay (1000);
+  digitalWrite (Pinprot, LOW);
+  pinMode(Pinestado, OUTPUT);
   pinMode(Pinrestado, OUTPUT);
-  
-  
 }
 void loop () {
   arranque = digitalRead(PinArranque);
@@ -51,14 +46,12 @@ void loop () {
     }
   }
 
-
   if (paro == HIGH) {
     digitalWrite (Pinrestado, HIGH);
     digitalWrite (Pinestado, HIGH);
     restado = 0;
     estado = 0;
   } delay (8);
-
 
   enclavamiento = estado;
   if (arranque == HIGH and w == HIGH) {
@@ -75,23 +68,18 @@ void loop () {
   if (estado == HIGH) {
     digitalWrite (Pinestado, LOW);
     digitalWrite (Pinrestado, HIGH);
- delay (10000);
- estado = 0;
-  } 
-  
- 
+    delay (10000);
+    estado = 0;
+  }
 
   else {
 
     digitalWrite (Pinestado, HIGH);
 
   }
+  digitalWrite (Pinestado, HIGH);
+  digitalWrite (Pinrestado, HIGH);
 
-   
-
-    digitalWrite (Pinestado, HIGH);
-    digitalWrite (Pinrestado, HIGH);
-    
   if (paro == HIGH) {
     estado = 0;
     restado = 0;
@@ -107,33 +95,29 @@ void loop () {
   if (restado == HIGH) {
     digitalWrite (Pinrestado, LOW);
     digitalWrite (Pinestado, HIGH);
-delay (10000);
- restado = 0;
+    delay (10000);
+    restado = 0;
   }
-
-  
 
   else {
 
     digitalWrite (Pinrestado, HIGH);
 
   }
-  
-    digitalWrite (Pinestado, HIGH);
-    digitalWrite (Pinrestado, HIGH);
-  
+
+  digitalWrite (Pinestado, HIGH);
+  digitalWrite (Pinrestado, HIGH);
+
   if (paro == HIGH) {
     digitalWrite (Pinrestado, HIGH);
     digitalWrite (Pinestado, HIGH);
     restado = 0;
     estado = 0;
-
   }
   if (reversible == HIGH and w == HIGH) {
     estado = 0;
     restado = 0;
   }
-
   if (arranque == HIGH and w == LOW) {
     estado = 0;
     restado = 0;
